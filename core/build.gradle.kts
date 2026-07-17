3868184ec5bc16bfb19ecdddef6cb3d8fd424600
Hendsayed25 <hendsayed853@gmail.com>
2026-07-17 16:20:23 +0300

    add navigation setup

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
@@ -25,6 +28,14 @@ android {
 dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
+
+    //navigation
+    implementation(libs.bundles.navigation3)
+
+    //di
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
