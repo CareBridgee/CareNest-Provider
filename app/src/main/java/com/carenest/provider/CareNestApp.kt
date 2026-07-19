7e7c4f370fb5666450d4a236c09dc87308f699a7
wahid <wahidqandill@gmail.com>
2026-07-19 10:23:09 +0300

    setup hilt and add registration screen

---
+++ app/src/main/java/com/carenest/provider/CareNestApp.kt
@@ -0,0 +1,7 @@
+package com.carenest.provider
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CareNestApp : Application()
--- app/src/main/java/com/carenest/provider/MainActivity.kt
+++ app/src/main/java/com/carenest/provider/MainActivity.kt
@@ -5,14 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.carenest.provider.designsystem.theme.SpTheme
+import com.carenest.provider.profile.presentation.ui.registration.RegistrationScreen
 import com.carenest.provider.ui.theme.CareNestProviderTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +20,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             CareNestProviderTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    SpTheme {
+                        RegistrationScreen(
+                            onNavigateToApplicationUnderReview = { TODO() }
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CareNestProviderTheme {
-        Greeting("Android")
     }
 }
