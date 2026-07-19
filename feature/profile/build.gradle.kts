c5b5d452c4cdbb6af076c5cbf87180078a8ea2f8
Hendsayed25 <hendsayed853@gmail.com>
2026-07-19 18:19:35 +0300

    add hilt dependencies

--- feature/profile/build.gradle.kts
+++ feature/profile/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
@@ -29,6 +32,7 @@ android {
 dependencies {
 
     implementation(project(":designsystem"))
+    implementation(project(":core"))
 
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
@@ -36,6 +40,12 @@ dependencies {
 
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.bundles.compose)
+    implementation(libs.bundles.coil)
+
+    //hilt
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.hilt.lifecycle.viewmodel.compose)
+    ksp(libs.hilt.compiler)
 
     // Lifecycle and ViewModel
     implementation(libs.bundles.lifecycle)
