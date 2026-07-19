c98edc17b4aebb9eff16de50d220c489b823d7d3
Hendsayed25 <hendsayed853@gmail.com>
2026-07-19 18:39:11 +0300

    move screen in right package

--- feature/profile/src/main/java/com/carenest/provider/profile/presentation/ui/under_review_screen/composable/ActionSection.kt
+++ feature/profile/src/main/java/com/carenest/provider/profile/presentation/ui/under_review_screen/composable/ActionSection.kt
@@ -1,4 +1,4 @@
-package com.carenest.provider.profile.presentation.completeprofile.under_review_screen.composable
+package com.carenest.provider.profile.presentation.ui.under_review_screen.composable
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
