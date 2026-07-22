a28902a61e838f9d70a981e711b460a6a1418f51
wahid <wahidqandill@gmail.com>
2026-07-22 12:28:44 +0300

    add datastore module

---
+++ core/src/main/java/com/carenest/provider/core/datastore/DataStoreModule.kt
@@ -0,0 +1,36 @@
+package com.carenest.provider.core.datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+private const val TOKEN_DATA_STORE_NAME = "auth_tokens"
+private val Context.tokenDataStore by preferencesDataStore(name = TOKEN_DATA_STORE_NAME)
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class TokenManagerModule {
+    @Binds
+    @Singleton
+    abstract fun bindTokenManager(
+        implementation: DataStoreTokenManager
+    ): TokenManager
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+    @Provides
+    @Singleton
+    fun provideTokenDataStore(
+        @ApplicationContext context: Context
+    ): DataStore<Preferences> = context.tokenDataStore
+}
