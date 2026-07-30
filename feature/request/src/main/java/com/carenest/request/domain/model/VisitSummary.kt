2dc316dae1f7dfd4eb3893ca02a0f36ab34ca040
Hendsayed25 <hendsayed853@gmail.com>
2026-07-30 17:41:52 +0300

    add visit summery model and repo

---
+++ feature/request/src/main/java/com/carenest/request/domain/model/VisitSummary.kt
@@ -0,0 +1,12 @@
+package com.carenest.request.domain.model
+
+
+data class VisitSummary(
+    val requestId: String,
+    val professionalName: String,
+    val serviceType: String,
+    val durationMinutes: Int,
+    val completedDate: String,
+    val totalAmount: Double,
+    val isVerified: Boolean,
+)
