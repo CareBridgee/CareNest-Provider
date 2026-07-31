dcb5fd5e734ec3180b5cf395a066542810451e36
Hendsayed25 <hendsayed853@gmail.com>
2026-07-31 17:39:26 +0300

    add usecaes

--- feature/request/src/main/java/com/carenest/request/domain/usecase/GetRequestContractUseCase.kt
+++ feature/request/src/main/java/com/carenest/request/domain/usecase/GetRequestContractUseCase.kt
@@ -1,14 +1,12 @@
 package com.carenest.request.domain.usecase
 
-
 import com.carenest.request.domain.model.Offer
-import com.carenest.request.domain.model.Request
 import com.carenest.request.domain.repository.NurseRequestsRepository
 import javax.inject.Inject
 
-class GetIncomingRequestsUseCase @Inject constructor(
+class GetRequestContractUseCase @Inject constructor(
     private val repository: NurseRequestsRepository,
 ) {
-    suspend operator fun invoke(): Result<List<Request>> =
-        runCatching { repository.fetchIncomingRequests() }
+    suspend operator fun invoke(requestId: String): Result<Offer> =
+        runCatching { repository.fetchRequestContract(requestId) }
 }
